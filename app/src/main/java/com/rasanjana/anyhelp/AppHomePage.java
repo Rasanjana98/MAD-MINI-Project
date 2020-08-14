16d9f7f66dacf300cb87540f25642e92d06055d0
Rasanjana Kuruppu <68512019+Rasanjana98@users.noreply.github.com>
2020-08-14 23:57:15 +0530

    Home Page

---
+++ app/src/main/java/com/rasanjana/anyhelp/AppHomePage.java
@@ -0,0 +1,14 @@
+package com.rasanjana.anyhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_home_page);
+    }
+}
--- app/src/main/java/com/rasanjana/anyhelp/UserRegister.java
+++ app/src/main/java/com/rasanjana/anyhelp/UserRegister.java
@@ -2,13 +2,29 @@ package com.rasanjana.anyhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserRegister extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+        button=findViewById(R.id.registerBtn);
+    }
+    @Override
+    protected  void onResume() {
+        super.onResume();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(UserRegister.this,AppHomePage.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
