249abb0fb2a25bd2e9a56203ecf4205da96d64b7
BuddhimaParakramage <buddhimasagc@gmail.com>
2020-08-15 00:49:08 +0530

    Nurse Career

--- app/src/main/java/com/rasanjana/anyhelp/NurseCareerCreate.java
+++ app/src/main/java/com/rasanjana/anyhelp/NurseCareerCreate.java
@@ -3,14 +3,12 @@ package com.rasanjana.anyhelp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class AppHomePage extends AppCompatActivity {
-    Button button;
+public class NurseCareerCreate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_home_page);
+        setContentView(R.layout.activity_nurse_career_create);
     }
 }
