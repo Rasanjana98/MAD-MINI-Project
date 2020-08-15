9b8d740be27f0a0eab1d33ea517fc5984860e7bf
ASUS <hd43190@gmail.com>
2020-08-15 14:14:23 +0530

    Action ba/ edit text wala borders dala athi e tika balanna

---
+++ app/src/main/java/com/rasanjana/anyhelp/AddAppointment.java
@@ -0,0 +1,24 @@
+package com.rasanjana.anyhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AddAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_appointment);
+
+        ImageView ivBack = findViewById(R.id.ivBack);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
--- app/src/main/java/com/rasanjana/anyhelp/MainActivity.java
+++ app/src/main/java/com/rasanjana/anyhelp/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
     Button button,button2;
 
     @Override
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.signup);
         button2=findViewById(R.id.login);
+
+        Log.i(TAG, "onCreate: ");
     }
     @Override
     protected  void onResume() {
