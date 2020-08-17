1249afe3244415586797ee2e09ed0ccfa569bea9
ASUS <hd43190@gmail.com>
2020-08-17 11:30:29 +0530

    MechanicEditProfile, MechanicRegistration, PaymentActivity, Strings and AndroidManifest.xml(Himantha Amarathunga's files)

---
+++ app/src/main/java/com/rasanjana/anyhelp/MechanicEditProfile.java
@@ -0,0 +1,14 @@
+package com.rasanjana.anyhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MechanicEditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanic_edit_profile);
+    }
+}
