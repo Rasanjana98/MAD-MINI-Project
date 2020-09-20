54605030b96829fdb32608aeae92df92d38d4956
Rasanjana Kuruppu <68512019+Rasanjana98@users.noreply.github.com>
2020-09-20 10:14:53 +0530

    login and signup

--- app/src/main/java/com/rasanjana/anyhelp/UserRegister.java
+++ app/src/main/java/com/rasanjana/anyhelp/UserRegister.java
@@ -1,21 +1,74 @@
 package com.rasanjana.anyhelp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserRegister extends AppCompatActivity {
+    EditText fName,lName,email,pNumber,password,cPassword;
     Button button;
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
         button=findViewById(R.id.registerBtn);
+        fName=findViewById(R.id.fName);
+        lName=findViewById(R.id.lName);
+        email=findViewById(R.id.email);
+        pNumber=findViewById(R.id.pNumber);
+        password=findViewById(R.id.password);
+        cPassword=findViewById(R.id.cPassword);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email=email.getText().toString().trim();
+                String Password=password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(Email)){
+                    email.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(Password)){
+                    password.setError("Password is required");
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(UserRegister.this,"User Created",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(UserRegister.this,"Error"+ task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         ImageView ivBack = findViewById(R.id.ivBack);
         ivBack.setOnClickListener(new View.OnClickListener() {
@@ -25,15 +78,15 @@ public class UserRegister extends AppCompatActivity {
             }
         });
     }
-    @Override
-    protected  void onResume() {
-        super.onResume();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent=new Intent(UserRegister.this,AppHomePage.class);
-                startActivity(myIntent);
-            }
-        });
-    }
+//    @Override
+//    protected  void onResume() {
+//        super.onResume();
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent myIntent=new Intent(UserRegister.this,AppHomePage.class);
+//                startActivity(myIntent);
+//            }
+//        });
+//    }
 }
