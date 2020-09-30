ac0204ccd730d68951026a033488b3a094426fed
ASUS <hd43190@gmail.com>
2020-09-30 13:29:07 +0530

    create user menu(AppHomePage, UserMenuActivity)

--- app/src/main/java/com/rasanjana/anyhelp/AppHomePage.java
+++ app/src/main/java/com/rasanjana/anyhelp/AppHomePage.java
@@ -4,23 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseAuth;
 
 public class AppHomePage extends AppCompatActivity {
-    Button button2,button3,button4;
+    Button btnTeacher, btnNurse, btnMechanic, btnPlumber, btnLogOut;
+    private static final String TAG = "AppHomePage";
+    public static final String profession = "profession";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_home_page);
 
-        button2=findViewById(R.id.button9);
-        button3=findViewById(R.id.button8);
-        button4=findViewById(R.id.logout);
+        btnTeacher = findViewById(R.id.teacherPro);
+        btnNurse = findViewById(R.id.nursePro);
+        btnMechanic = findViewById(R.id.mechanicPro);
+        btnPlumber = findViewById(R.id.plumberPro);
+        btnLogOut = findViewById(R.id.logout);
+
+        Log.i(TAG, "btnTeacher: "+btnTeacher);
+
         ImageView ivBack = findViewById(R.id.ivBack);
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,31 +37,47 @@ public class AppHomePage extends AppCompatActivity {
                 onBackPressed();
             }
         });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            }
-        });
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(AppHomePage.this, TeacherProfile.class);
-                startActivity(myIntent);
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent=new Intent(AppHomePage.this,NurseProfile.class);
-                startActivity(myIntent);
-            }
-        });
+//        btnLogOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut();
+//                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//            }
+//        });
 
+        btnTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AppHomePage.this, UserMenuActivity.class);
+                String Profession = "teacher";
+                intent.putExtra(profession, Profession);
+                startActivity(intent);
+            }
+        });
+        btnNurse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AppHomePage.this, UserMenuActivity.class);
+                String Profession = "nurse";
+                intent.putExtra(profession, Profession);
+                startActivity(intent);
+            }
+        }); btnMechanic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AppHomePage.this, UserMenuActivity.class);
+                String Profession = "mechanic";
+                intent.putExtra(profession, Profession);
+                startActivity(intent);
+            }
+        }); btnPlumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AppHomePage.this, UserMenuActivity.class);
+                String Profession = "plumber";
+                intent.putExtra(profession, Profession);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/rasanjana/anyhelp/UserMenuActivity.java
+++ app/src/main/java/com/rasanjana/anyhelp/UserMenuActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,73 +26,167 @@ public class UserMenuActivity extends AppCompatActivity {
 
     DatabaseReference dbRef;
     private List<Plumber> plumbers;
+    private List<Teacher> teachers;
+    //private List<Mechanic> mechanics;
+    private List<Nurse> nurses;
+    private String profession = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_menu);
 
-       /* ImageView ivBack = findViewById(R.id.ivBack);
+        ImageView ivBack = findViewById(R.id.ivBack);
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
-        });*/
+        });
 
         plumbers = new ArrayList<>();
-        /*Plumber plumber1 = new Plumber();
-        plumber1.setName("Hashan");
-        plumber1.setLocation("Gampaha");
-        plumber1.setAvailableTime("Day");
+        teachers = new ArrayList<>();
+       // mechanics = new ArrayList<>();
+        nurses = new ArrayList<>();
 
-        Plumber plumber2 = new Plumber();
-        plumber2.setName("Shashika");
-        plumber2.setLocation("Colombo");
-        plumber2.setAvailableTime("Night");
+//get buttons from appHome
+        Intent intent = getIntent();
+      profession = intent.getStringExtra(AppHomePage.profession);
 
-        Plumber plumber3 = new Plumber();
-        plumber3.setName("iii");
-        plumber3.setLocation("Kaluthara");
-        plumber3.setAvailableTime("Day/night");
+            Log.i(TAG, "profession: "+ profession);
 
-        plumbers.add(plumber1);
-        plumbers.add(plumber2);
-        plumbers.add(plumber3);*/
-
-        final PlumberUserMenuAdapter adapter = new PlumberUserMenuAdapter(this);
-        adapter.setPlumbers(plumbers);
-        adapter.setOnItemClickListener(new PlumberUserMenuAdapter.OnItemClickListener() {
+        final PlumberUserMenuAdapter plumberAdapter = new PlumberUserMenuAdapter(this);
+        plumberAdapter.setPlumbers(plumbers);
+        plumberAdapter.setOnItemClickListener(new PlumberUserMenuAdapter.OnItemClickListener() {
             @Override
             public void onItemClicked(int position) {
                 Log.i(TAG, "onItemClicked: position = "+position);
             }
         });
-
-        RecyclerView rvPlumbers = findViewById(R.id.rvPlumbers);
-        rvPlumbers.setAdapter(adapter);
-        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(UserMenuActivity.this, LinearLayoutManager.VERTICAL, false);
-        rvPlumbers.setLayoutManager(horizontalLayoutManager);
-
-        DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Plumber");
-        readRef.addValueEventListener(new ValueEventListener() {
+        final TeacherUserMenuAdapter teacherAdapter = new TeacherUserMenuAdapter(this);
+        teacherAdapter.setTeachers(teachers);
+        teacherAdapter.setOnItemClickListener(new TeacherUserMenuAdapter.OnItemClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Log.i(TAG ,"Count = " + dataSnapshot.getChildrenCount());
-                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                    Log.i(TAG, "onDataChange: key = "+postSnapshot.getKey());
-                    Plumber plumber = postSnapshot.getValue(Plumber.class);
-                    plumbers.add(plumber);
-                }
-
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onItemClicked(int position) {
+                Log.i(TAG, "onItemClicked: position"+position);
 
             }
         });
+//        final MechanicUserMenuAdapter mechanicAdapter = new MechanicUserMenuAdapter(this);
+//        mechanicAdapter.setMechanics(mechanics);
+//        mechanicAdapter.setOnItemClickListener(new MechanicUserMenuAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClicked(int position) {
+//                Log.i(TAG, "onItemClicked: position"+position);
+//            }
+//        });
+        final NurseUserMenuAdapter nurseAdapter = new NurseUserMenuAdapter(this);
+        nurseAdapter.setNurses(nurses);
+        nurseAdapter.setOnItemClickListener(new NurseUserMenuAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                Log.i(TAG, "onItemClicked: position"+position);
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(UserMenuActivity.this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+
+        switch (profession){
+            case "teacher":
+                recyclerView.setAdapter(teacherAdapter);
 
 
+                DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("Teacher");
+                readRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Log.i(TAG ,"Count = " + dataSnapshot.getChildrenCount());
+                        for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                            Log.i(TAG, "onDataChange: key = "+postSnapshot.getKey());
+                            Teacher teacher = postSnapshot.getValue(Teacher.class);
+                            teachers.add(teacher);
+                        }
+
+                        teacherAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                break;
+
+            case "nurse":
+                recyclerView.setAdapter(nurseAdapter);
+                DatabaseReference readRef2 = FirebaseDatabase.getInstance().getReference().child("Nurse");
+                readRef2.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Log.i(TAG ,"Count = " + dataSnapshot.getChildrenCount());
+                        for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                            Log.i(TAG, "onDataChange: key = "+postSnapshot.getKey());
+                            Nurse nurse = postSnapshot.getValue(Nurse.class);
+                            nurses.add(nurse);
+                        }
+
+                        nurseAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                break;
+
+//            case "mechanic":
+//                recyclerView.setAdapter(mechanicAdapter);
+//                DatabaseReference readRef3 = FirebaseDatabase.getInstance().getReference().child("Mechanic");
+//                readRef3.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        Log.i(TAG ,"Count = " + dataSnapshot.getChildrenCount());
+//                        for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+//                            Log.i(TAG, "onDataChange: key = "+postSnapshot.getKey());
+//                            Mechanic mechanic = postSnapshot.getValue(Mechanic.class);
+//                            mechanics.add(mechanic);
+//                        }
+//
+//                        mechanicAdapter.notifyDataSetChanged();
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                    }
+//                });
+//                break;
+            case "plumber":
+                recyclerView.setAdapter(plumberAdapter);
+                DatabaseReference readRef4 = FirebaseDatabase.getInstance().getReference().child("Plumber");
+                readRef4.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Log.i(TAG ,"Count = " + dataSnapshot.getChildrenCount());
+                        for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                            Log.i(TAG, "onDataChange: key = "+postSnapshot.getKey());
+                            Plumber plumber = postSnapshot.getValue(Plumber.class);
+                            plumbers.add(plumber);
+                        }
+
+                        plumberAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                break;
+        }
     }
 }
