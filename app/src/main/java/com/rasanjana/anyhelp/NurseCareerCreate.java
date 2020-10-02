54abfb351ebd4218081de674c5f146345bfa15d7
Buddhima Parakramage <buddhimasagc@gmail.com>
2020-10-02 19:17:28 +0530

    Nurse Class updated code

--- app/src/main/java/com/rasanjana/anyhelp/NurseCareerCreate.java
+++ app/src/main/java/com/rasanjana/anyhelp/NurseCareerCreate.java
@@ -156,7 +156,7 @@ public class NurseCareerCreate extends AppCompatActivity {
                         nurse.setServiceCategories(serviceCategories);
 
                         dbRef.push().setValue(nurse);
-                        Toast.makeText(getApplicationContext(),"Data saved Successfully",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext()," saved Successfully",Toast.LENGTH_SHORT).show();
                     }
 //                    public void onRadioButtonClicked(View view){
 //                        boolean checked =((RadioButton) view).isChecked();
